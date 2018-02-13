ea9a2e36ada95327893a9e078c23df7655f6dd95
Fabien Gréard <fgreard.ext@orange.com>
2018-02-13 15:16:36 +0100

    init

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express'),
+      path = require('path'),
+      logger = require('morgan'),
+      app = express()
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// logger init
+app.use(logger('dev'));
+
+//use index
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
